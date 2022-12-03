e54048dc870d0720c7bdb1036b6b003d3b9e0966
Adam Doamński <s13075@pjwstk.edu.pl>
2022-12-03 18:22:30 +0100

    redux dodany

--- logreviewer/src/index.jsx
+++ logreviewer/src/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
 import './index.css';
 import App from './component/App';
 import reportWebVitals from './reportWebVitals';
+import reducers from './module';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={
+    createStoreWithMiddleware(
+      reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__
+      && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+    >
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ logreviewer/src/module/index.js
@@ -0,0 +1,5 @@
+import {combineReducers} from "redux";
+
+export default combineReducers({
+    applicationReducers: () => [1,2,3,4]
+})
